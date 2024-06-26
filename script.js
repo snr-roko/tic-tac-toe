68079797dbdfebd4f4ab2aeb980afe8986e6976c
mr-r <rbbagyei@gmail.com>
2024-06-26 01:48:45 +0000

    tictactoe playable in console: Generate combinations and checkvalidity functions removed to incorporate an every method on array in validate array function; simplifying game patterns check. Also Gameboard and patterns have all been tucked into an IIFE. Game needs a check for already chosen numbers.

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// cells
-const gameboard = [1,2,3,4,5,6,7,8,9];
-
-// players object
-const players = {
-  player1: [],
-  player2: []
-}
-
-// game winning patterns
+// An IIFE to tuck away gameboard pieces
+const gameboard = (() => {
+  const gameboard = [1,2,3,4,5,6,7,8,9];
+  // game winning patterns
 const patterns = [
   [1,2,3],
   [4,5,6],
@@ -16,65 +10,71 @@ const patterns = [
   [2,5,8],
   [3,6,9],
   [1,5,9],
-  [3,5,7],
-  [1,2,3].reverse(),
-  [4,5,6].reverse(),
-  [7,8,9].reverse(),
-  [1,4,7].reverse(),
-  [2,5,8].reverse(),
-  [3,6,9].reverse(),
-  [1,5,9].reverse(),
-  [3,5,7].reverse()
+  [3,5,7]
 ]
+
+  return {gameboard, patterns};
+})();
+
+
+// players object
+const players = {
+  player1: [],
+  player2: []
+}
+
+
+
 
 
 // A function to remove a particular cell chosen by a player
 // And pushes that cell to his array of choices
 function choose(player) {
-  const choice = prompt('Choose a cell').parseInt();
+  const choice = parseInt(prompt("Choose your cell, "), 10);
   player.push(choice);
-  index = gameboard.indexOf(choice);
-  gameboard.splice(index, 1);
+  index = gameboard.gameboard.indexOf(choice);
+  gameboard.gameboard.splice(index, 1);
 }
 
 // A function to check if a pattern has been matched 
 function checkWinner(player, pattern) {
-  const combinations = generateCombinations(player);
-  const length = pattern.length;
-  for (let i = 0; i < combinations.length; i++) {
-    for (let j = 0; j < length; j++) {
-      if (validateArrays(player[i], pattern[j])) {
-        return true;
-      } else {
-      return false;
+  if(player.length < 3) return false;
+
+  for(let i = 0; i < pattern.length; i ++) {
+    if(validateArrays(player, pattern[i])) return true;
+  }
+
+  return false;
+}
+
+// A function to check if all values in a pattern can be found in a player's array
+function validateArrays(array1, array2) {
+  return array2.every((value) => array1.includes(value));
+}
+
+
+// The Game function
+function tictactoe() {
+  while(true) {
+    if(gameboard.gameboard.length === 0) {
+      console.log("It is a tie!!!");
+      break;
+    }
+    choose(players.player1);
+    if(checkWinner(players.player1, gameboard.patterns)) {
+      console.log("Player1 wins!!!");
+      break;
+    } else {
+      if(gameboard.gameboard.length === 0) {
+        console.log("It is a tie!!!");
+        break;
+      }
+      choose(players.player2);
+      if(checkWinner(players.player2, gameboard.patterns)) {
+        console.log("Player2 wins!!!");
+        break;
+      }
     }
   }
-
+    
 }
-}
-
-// A function to check if two arrays are equal
-function validateArrays(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } else return true;
-  }
-}
-
-
-// A function to generate all possible combinations of elements in an array
-function generateCombinations(player) {
-  length = player.length;
-  combinations = [];
-  for (let i = 0; i < length; i++) {
-    for (let j = i + 1; j < length; j++) {
-      for (let k = j + 1; k < length; k++) {
-        combinations.push([player[i], player[j], player[k]]);
-      }
-  }
-}
-  return combinations;
-}
-
-
